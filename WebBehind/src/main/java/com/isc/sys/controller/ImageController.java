be66bf8529a3aa568177d1ca9eb0eae1816fe96c
711lxsky <929570291@qq.com>
2023-06-28 15:23:40 +0800

    图片上传和展示测试，超长字段测试
    正在构思表重建和项目重构，整体框架构建

---
+++ WebBehind/src/main/java/com/isc/sys/controller/ImageController.java
@@ -0,0 +1,54 @@
+package com.isc.sys.controller;
+
+import com.isc.common.viewObj.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+
+    @Value("${file-save-path}")
+    private String fileSavePath;
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd-");
+
+    @PostMapping("/upload")
+    public Result uploadImage(@RequestParam("file")MultipartFile file, HttpServletRequest request){
+        String directory = simpleDateFormat.format(new Date());
+        System.out.println(directory);
+        File dir = new File(fileSavePath);
+        if (! dir.exists()){
+            dir.mkdir();
+            System.out.println("create this file path");
+        }
+        System.out.println("图片保存位置："+fileSavePath);
+        String suffix = ".jpg";
+        System.out.println(suffix);
+        String newFileName = directory + UUID.randomUUID() + suffix;
+        System.out.println(newFileName);
+        File newFile = new File(fileSavePath  + newFileName);
+        try {
+            file.transferTo(newFile);
+            String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/image/" + newFileName;
+            System.out.println("url地址："+url);
+            return Result.success("上传成功"+url);
+        }
+        catch (IOException e){
+            return Result.fail("传不了！");
+        }
+    }
+
+}
